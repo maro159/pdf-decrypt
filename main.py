f0c202609818db0461622523ccb3ca68dcbf2976
mrcm777@gmail.com <mrcm777@gmail.com>
2022-10-25 02:13:07 +0200

    add non recursive option

--- main.py
+++ main.py
@@ -3,31 +3,46 @@ import os
 import sys
 from PyPDF2 import PdfFileReader
 
+def process(filepath):
+    if filepath.endswith(".pdf"):
+        pdfFile = PdfFileReader(filepath)
+        if pdfFile.is_encrypted:
+            print("[Processing]", filepath)
+            cmd = "copy \"" + filepath + "\" temp.pdf >nul"
+            print(cmd)
+            os.system(cmd)
+            cmd = "qpdf --password=" + password + " --decrypt temp.pdf \"" + filepath + "\""
+            print(cmd)
+            os.system(cmd)
+            cmd = "del temp.pdf /s /q"
+            print(cmd)
+            os.system(cmd)
+            print("[Finished]", filepath, 'File Decrypted (qpdf)')
+        else:
+            pass
+            print("[Finished]", filepath, 'File Not Encrypted')
 
-fileDir = sys.argv[1]
+
+fileDir = "./"
+password = ""
+recursive = False
+
+if len(sys.argv) > 1:
+    fileDir = sys.argv[1]
 if len(sys.argv) > 2:
     password = sys.argv[2]
+if len(sys.argv) > 3:
+    if sys.argv[3] == "recursive":
+        recursive = True
 
-print(fileDir)
-print(password)
-
-for r, d, f in os.walk(fileDir):
-    for file in f:
-        if file.endswith(".pdf"):
+if (recursive):
+    for r, d, f in os.walk(fileDir):
+        for file in f:
             filepath = os.path.join(r, file)
-            pdfFile = PdfFileReader(filepath)
-            if pdfFile.is_encrypted:
-                print("[Processing]", filepath)
-                cmd = "copy \"" + filepath + "\" temp.pdf >nul"
-                print(cmd)
-                os.system(cmd)
-                cmd = "qpdf --password=" + password + " --decrypt temp.pdf \"" + filepath + "\""
-                print(cmd)
-                os.system(cmd)
-                cmd = "del temp.pdf /s /q"
-                print(cmd)
-                os.system(cmd)
-                print("[Finished]", filepath, 'File Decrypted (qpdf)')
-            else:
-                pass
-                print("[Finished]", filepath, 'File Not Encrypted')
+            process(filepath)   
+else:
+    for item in os.scandir():
+            if item.is_file():
+                process(item.path)
+
+
